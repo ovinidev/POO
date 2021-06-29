62b4c04a4e8f1253655d91f89077167d64b19cc2
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-06-28 21:34:19 -0300

    primeiro programa em c++

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Time {
+  public:
+    string nome;
+    string cor;
+    string estado;
+    int titulos;
+
+    void mostrarTime();
+
+};
+
+void Time::mostrarTime(){
+  this -> nome = "Flamengo";
+  this -> cor = "Vermelho";
+  this -> estado = "Rio";
+  this -> titulos = 7;
+
+};
+
+int main() {
+
+  Time *time1 = new Time();
+
+  time1 -> mostrarTime();
+
+  cout << time1 -> nome << endl;
+  cout << time1 -> cor << endl;
+  cout << time1 -> estado << endl;
+  cout << time1 -> titulos << endl;
+
+
+  return 0;
+}
