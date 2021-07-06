a3d2879f4af126da12e8f9a2d16a1e1443d9d740
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-07-05 22:37:28 -0300

    finalizando com polimorfismo

--- Estudos/CodeBeauty.cpp
+++ Estudos/CodeBeauty.cpp
@@ -40,48 +40,43 @@ class Employee:AbstractEmployee{
 
     void IntroduceYourself();
 
-    Employee(string name, string company, int age);
+    Employee(string name, string company, int age); // Constructor
 
     void AskForPromotion(){
-      if(Age > 30)
-        cout << Name << " Got promoted!" << endl;
+      if(Age >= 30)
+        cout << getName() << " Got promoted!" << endl;
       else
-        cout << Name << " Not promoted!" << endl;
+        cout << getName() << " Not promoted!" << endl;
     };
+
+    virtual void Work() {
+      cout << getName() << " is checking email" << endl;
+    }
 };
 
 class Developer:public Employee{
   public:
     string Languages;
 
+    void setLanguage(string language){
+      Languages = language;
+    };
+    string getLanguage(){
+      return Languages;
+    };
+
     Developer(string name, string company, int age, string languages)
     :Employee(name, company, age){
       Languages = languages;
-    }
+    };
     
     void FixBug(){
       cout << getName() << " fixed bug using " << Languages << endl; 
-    }
-};
+    };
 
-class Engineer:public Employee{
-  private:
-    int Salary;
-
-  public:
-    void RepairEquipment(){
-      cout << getName() << " Concerted" << endl;
-    
+    void Work() {
+      cout << getName() << " is programing now in " << getLanguage() << endl;
     }
-    void ShowSalary(){
-      cout << Salary << endl;
-    }
-
-    Engineer(string name, string company, int age, int salary)
-    :Employee(name, company, age){
-      Salary = salary;
-    }
-
 };
 
 void Employee::IntroduceYourself(){
@@ -97,20 +92,52 @@ Employee::Employee(string name, string company, int age){
   Age = age;
 };
 
+class Engineer:public Employee{
+  protected:
+    int Salary;
 
+  public:
+    void RepairEquipment(){
+      cout << getName() << " Concerted" << endl;
+    
+    };
+    int getSalary(){
+      return Salary;
+    };
+
+    Engineer(string name, string company, int age, int salary)
+    :Employee(name, company, age){
+      Salary = salary;
+    };
+
+    void Work() { // Quando uma função virtual é invocada, ela fica globalmente
+      cout << getName() << " is geting $" << getSalary() << " dolars" << endl;
+    };
+
+};
 
 
 int main(){
 
-  Employee employee1 = Employee("Vini", "Amazon", 16);
+  Employee employee1 = Employee("Vini", "Tabajaras", 23);
   
-  employee1.AskForPromotion();
-
   Developer dev1 = Developer("Vick", "Spotify", 19, "python");
-  dev1.FixBug();
-  dev1.IntroduceYourself();
 
   Engineer eng1 = Engineer("Arthur", "Netflix", 29, 55);
-  eng1.IntroduceYourself();
-  eng1.RepairEquipment();
+
+
+  Employee *e1 = &dev1;
+  Employee *e2 = &eng1;
+
+  e1 -> Work();
+  e2 -> Work();
+
+  e1 -> setAge(30);
+  e1 -> getAge();
+  e1 -> setName("oi");
+  e1 -> getName();
+
+  eng1.setName("Vini");
+  eng1.getName();
+
 }
