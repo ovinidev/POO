36ad4ed25e864d76cc8ed2eb8ec8529e8bddacda
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-09-21 21:00:03 -0300

    finish poo

--- Atividades/8/main.cpp
+++ Atividades/8/main.cpp
@@ -13,6 +13,8 @@ int main()
   Pilha<int> pi;
   int x = 1;
 
+  cout << "Inserindo elementos..." << endl;
+
   pi.push(x);
   pi.push(x);
   pi.push(x);
@@ -24,8 +26,11 @@ int main()
   pi.push(x);
   pi.push(x);
 
+  cout << "Exibindo elementos: " << endl;
   pi.print();
 
+  cout << "Removendo elementos..." << endl;
+
   pi.pop(x);
   pi.pop(x);
   pi.pop(x);
@@ -36,6 +41,8 @@ int main()
   pi.pop(x);
   pi.pop(x);
   pi.pop(x);
+
+  cout << "Tentando remover elementos com a pilha vazia: " << endl;
   
   pi.pop(x);
   pi.pop(x);
